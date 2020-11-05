560a5371fa115fcc2f4aa881019eeb4aa201cb8f
grammlaman <grammlaman@gmail.com>
2020-11-05 17:51:34 +0600

    init: start of works

--- dev/js/main.js
+++ dev/js/main.js
@@ -5,9 +5,19 @@ class Page {
 		_.header = _.body.querySelector('header');
 		_.burgerCondition = false;
 
-		document.querySelector('.head-burger-btn').addEventListener('click',_.burgerClick)
+		document.querySelector('.head-burger-btn').addEventListener('click',function () {
+			_.burgerClick();
+		});
+		document.querySelectorAll('.head-burger-link').forEach(function (btn,int) {
+			if (int > 0){
+				btn.addEventListener('click',function () {
+					_.burgerClick();
+				})
+			}
+		});
 
 		_.init();
+		window.addEventListener('scroll',_.headScroll);
 	}
 
 	createEl(tag,cls = null,data){
@@ -23,6 +33,10 @@ class Page {
 		return temp;
 	}
 
+	headScroll(){
+		let header = document.querySelector('.head');
+		window.scrollY > 0 ? header.classList.add('active') : header.classList.remove('active')
+	}
 	burgerClick(){
 		const _ = this;
 
@@ -32,24 +46,28 @@ class Page {
 		let oneTime = 100;
 		let time = oneTime * headLinks.length;
 
-
-		let int = _.burgerCondition ? headLinks.length - 1 : 0;
-		let interval = setInterval(function () {
-			if (!_.burgerCondition){
+		let interval;
+		if (!_.burgerCondition) {
+			let int = 0;
+			interval = setInterval(function () {
 				headLinks[int].classList.add('active');
 				int++;
-			} else {
+			}, oneTime);
+			_.burgerCondition = true;
+			burgerBtn.classList.add('active');
+		} else {
+			let int = headLinks.length - 1;
+			interval = setInterval(function () {
 				headLinks[int].classList.remove('active');
 				int--;
-			}
-		}, oneTime);
-
-		burgerBtn.classList.toggle('active');
-		_.burgerCondition = !_.burgerCondition;
+			},oneTime);
+			_.burgerCondition = false;
+			burgerBtn.classList.remove('active');
+		}
 
 		setTimeout(function (e) {
 			clearInterval(interval)
-		},time)
+		},time);
 	}
 
 	sliderPrepare(){
@@ -63,20 +81,71 @@ class Page {
 
 		let slides = document.querySelectorAll('.slide');
 		slides.forEach(function (el,index) {
-			let button = document.createElement('BUTTON');
-			button.append(document.createElement('SPAN'));
-			button.className = 'slider-control-btn';
-			if (index === 0) button.classList.add('active');
+			let button = _.createEl('BUTTON','slider-control-btn');
+			button.append(_.createEl('SPAN'));
+			if (index === 0) {
+				button.classList.add('active');
+				el.classList.add('active');
+			}
 			section.append(button);
+			button.addEventListener('click',function () {
+				_.swapSlide(index);
+			})
+		})
+	}
+	swapSlide(index){
+		const _ = this;
+
+		let btns = document.querySelectorAll('.slider-control-btn');
+		let slides = document.querySelectorAll('.slide');
+
+		btns.forEach(function (btn,int) {
+			if (btn.classList.contains('active')){
+				btn.classList.remove('active');
+				slides[int].classList.remove('active');
+			}
+		});
+		btns[index].classList.add('active');
+		slides[index].classList.add('active');
+	}
+
+	formContinue(){
+		const _ = this;
+
+		let btns = document.querySelectorAll('.calc-continue');
+		btns.forEach(function (el) {
+			el.addEventListener('click',function (e) {
+				let btn = e.target;
+				if (btn.textContent === 'Далее'){
+					let page = btn.parentElement;
+					page.style = 'margin-left:-100%';
+				}
+			})
+		})
+	}
+	formTypeChoose(){
+		const _ = this;
+
+		let btns = document.querySelectorAll('.calc-select-buttons button');
+		let input = document.querySelector('.calc-select-buttons input');
+		btns.forEach(function (el) {
+			el.addEventListener('click',function (e) {
+				btns.forEach(function (btn) {
+					btn.classList.remove('active');
+				});
+				el.classList.add('active');
+				input.value = el.getAttribute('data-value');
+			})
 		})
 	}
 
 
-
-
 	init(){
 		const _ = this;
-		_.sliderPrepare()
+		_.headScroll();
+		_.sliderPrepare();
+		_.formContinue();
+		_.formTypeChoose();
 	}
 }
 
